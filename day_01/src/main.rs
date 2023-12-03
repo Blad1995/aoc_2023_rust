8e491a74541e2328132541ce82798ecc759391e9
Ondřej Divina <ondrej.divina@kiwi.com>
2023-12-03 20:18:55 +0100

    feat(day_01): implement part 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,23 +1,53 @@
+use std::collections::HashMap;
 use std::fs;
+use std::iter::Iterator;
 
 fn main() {
-    let input_name = "input/input_1.txt";
+    let mapping: HashMap<&str, &str> = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+
+    let input_name = "input/input_2.txt";
     let file_data = fs::read_to_string(input_name).unwrap();
     let lines = file_data.lines();
+
     let mut total_sum: u32 = 0;
     for line in lines {
         let mut first_num: Option<char> = Option::None;
         let mut last_num: Option<char> = Option::None;
-        for letter in line.chars() {
-            if letter.is_ascii_digit() {
+        let mut i = 0;
+        while i < line.len() {
+            if line.chars().nth(i).unwrap().is_ascii_digit() {
                 match first_num {
-                    Some(_) => last_num = Some(letter),
+                    Some(_) => last_num = Some(line.chars().nth(i).unwrap()),
                     None => {
-                        first_num = Some(letter);
+                        first_num = Some(line.chars().nth(i).unwrap());
                         last_num = first_num;
                     }
                 }
+            } else {
+                for (key, value) in mapping.iter() {
+                    if line[i..].starts_with(key) {
+                        match first_num {
+                            Some(_) => last_num = Some(value.chars().next().unwrap()),
+                            None => {
+                                first_num = Some(value.chars().next().unwrap());
+                                last_num = first_num;
+                            }
+                        }
+                        break;
+                    }
+                }
             }
+            i += 1;
         }
         let line_number = format!(
             "{}{}",
